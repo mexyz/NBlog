5148b985923675f8e153b9a2660fd81f6e1bbf4a
zhangyixin <xyz@ubicast.com.cn>
2023-11-24 15:41:50 +0800

    update config

--- blog-cms/src/util/request.js
+++ blog-cms/src/util/request.js
@@ -4,7 +4,7 @@ import 'nprogress/nprogress.css'
 import {Message} from 'element-ui'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/admin/',
+	baseURL: 'http://api.zadker.com/admin/',
 	timeout: 5000
 })
 
--- blog-view/src/plugins/axios.js
+++ blog-view/src/plugins/axios.js
@@ -3,7 +3,7 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/',
+	baseURL: 'http://api.zadker.com/',
 	timeout: 10000,
 })
 
